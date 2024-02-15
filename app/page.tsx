f1ca9ce7b97711f3a5933ccd8f023a27273fb9e8
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-15 01:31:56 +0100

    Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import { PageInfo, Project, Skill, Social } from "@/typings";
 
-export default function Home() {
+
+type Props = {
+  pageInfo: PageInfo;
+  skills: Skill[];
+  projects: Project[];
+  socials: Social[];
+}
+export default function Home({pageInfo, projects, skills, socials}: Props) {
   return (
     <main className="">
 
       {/* Hero */}
+      <section id="hero" className="snap-center">
+        <Hero pageInfo={pageInfo}/>
+      </section>
 
       {/* About */}
 
---
+++ components/Hero.tsx
@@ -0,0 +1,10 @@
+import { PageInfo } from "@/typings";
+import React from "react";
+
+type Props = { pageInfo: PageInfo };
+
+const Hero = (props: Props) => {
+  return <div>Hero</div>;
+};
+
+export default Hero;
