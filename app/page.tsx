2db406e7fd61715bc7d6986350ab7f6d069e20dd
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-15 02:48:52 +0100

    gloval css

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ type Props = {
 };
 export default function Home({ pageInfo, projects, skills, socials }: Props) {
   return (
-    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll  z-0 overflow-x-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Header />
 
       {/* Hero */}
